682ec82125c3fdd99328b733f494556d85b71810
Mark Rego <rego121@gmail.com>
2019-08-20 14:08:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-
+planeteer_calls = %w[earth wind fire water heart]
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
